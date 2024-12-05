6fe5116bd3387a90c22801b549b6d8dfea6b20b6
moinul-ilm10 <moinulredorangecom@gmail.com>
2024-12-05 15:34:47 +0600

    Courses template created

---
+++ wp-content/themes/rednews/template-parts/courses.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Courses Page
+ *
+ * Dynamic Courses Page template for RedNews theme
+ * Allows full content management through WordPress page editor
+ */
+
+get_header();
+?>
+
+
+
+
+
+<?php
+get_footer();
+?>
