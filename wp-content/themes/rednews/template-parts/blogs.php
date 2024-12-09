e32145f184eb2b895bf020a7d4f8cd2fcf2a6719
moinul-ilm10 <moinulredorangecom@gmail.com>
2024-12-09 18:05:48 +0600

    add blog page template

---
+++ wp-content/themes/rednews/template-parts/blogs.php
@@ -0,0 +1,214 @@
+<?php
+
+/**
+ * Template Name: Blogs Page
+ *
+ * Dynamic Blogs Page template for RedNews theme
+ * Allows full content management through WordPress page editor
+ */
+
+get_header();
+?>
+
+
+<section id="blogsPage">
+    <div class="blog-page-container">
+        <h3 class="blogs-page-heading">Latest Blogs</h3>
+        <div class="blog-page-content">
+            <!-- blogs sidebar  -->
+            <aside class="blog-page-category">
+                <h4 class="blog-category-heading">Categories</h4>
+                <ul class="blog-category-list">
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                    <li>Category 1</li>
+                </ul>
+            </aside>
+            <div class="course-grid">
+                <div class="blog-post-container">
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog-card">
+                        <div class="card-img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/blog/card_img.png">
+                            <div class="card-label">Category</div>
+                        </div>
+                        <div class="card-content">
+                            <h3 class="card-heading">
+                                Title goes here
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit
+                                amet
+                                condimentum porttitor. Integer
+                                augue urna, volutpat in sapien a, convallis commodo quam.</p>
+                            <div class="card-footer">
+                                <p>24/04/2022</p>
+                                <p>By: Ahmad Raza</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-page-btn"><button type="submit">Read More</button></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+get_footer();
+?>
